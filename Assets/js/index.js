4f2c59b2bd82a9b49f5bc48fba3e2db833cced22
tt4623 <sltebr33zy@gmail.com>
2020-10-18 23:33:22 -0500

    added much logic to index.js

---
+++ Assets/js/index.js
@@ -0,0 +1,148 @@
+// save html elements in variables for easier access
+let startBtnEl = document.querySelector('#start-quiz');
+let timeEl = document.querySelector('#time');
+let questionEl = document.querySelector('#quiz-screen');
+let answersEl = document.querySelector('#answers');
+let unSubmitEl = document.querySelector('#submit-userName');
+let usernameEl = document.querySelector('#userName');
+let startScreenEl = document.querySelector('#start-screen');
+let alertEl = document.querySelector('#alert');
+
+
+// questions object
+let Questions = [{
+    'questionHead':'Temporary question!! What is your favorite color? Hope its not shitty!',
+    'answers':['shit-brown', 'diarrea-green', 'piss-yellow', 'ass-pink'],
+    'correctAnswer':'shit-brown'
+}, {
+    'questionHead':'Temporary question!! What is your favorite color? Hope its not shitty!',
+    'answers':['shit-brown', 'diarrea-green', 'piss-yellow', 'ass-pink'],
+    'correctAnswer':'shit-brown'
+}];
+
+// initiate interval ID function to access throughout
+let interval;
+// assuming 5 questions 5x20
+let timeLeft = 100;
+
+// start the game when start button is pressed
+startBtnEl.addEventListener('click', function(event) {
+    event.preventDefault();
+    // hide start screen
+    startScreenEl.setAttribute('class', 'notCurrent');
+
+    // set questions screen to current screen
+    questionEl.setAttribute('class', 'current');
+
+    // push STARTING time to counter, only happens once
+    timeEl.textContent = timeLeft;
+
+    // start the timer and set interval to tic every second
+    interval = setInterval(function(){
+
+        // time counting down by interval of 1 second
+        timeLeft--;
+        
+        // changing timer in corner every second
+        timeEl.textContent = timeLeft;
+
+        if(timeLeft <= 0) {
+            end();
+        }
+
+    }, 1000);
+
+    // call function to handle question page and get question
+    questionPage();
+});
+
+
+// !!! create a function to handle attributes in question page function! !!!
+
+// to allow grabbing of FIRST current question
+let questionIndex = 0;
+
+// function to handle questions div and draw from 'questions' object
+function questionPage() {
+
+    // re assigning element to object
+    let qHeaderEl = document.querySelector('#qHead');
+    // push qHeader to screen (in qHeaderEl)
+    qHeaderEl.textContent = Questions[questionIndex].questionHead;
+
+    // get rid of last questions, or they'll stack, really took awhile to figure out why lol
+    answersEl.textContent = '';
+
+    // setting answer choices
+    for(let i = 0; i < Questions[questionIndex].answers.length; i++) {
+        let choiceBtn = document.createElement('button');
+        choiceBtn.setAttribute('class', 'inline-button');
+        // set value to answer in button
+        choiceBtn.setAttribute('value', `${Questions[questionIndex].answers[i]}`)
+        // create a varibale to pass through onclick attrbt so we can access arg in function
+        let answrNum = `btnClick(${i})`
+        choiceBtn.setAttribute('onclick', answrNum);
+        choiceBtn.textContent = Questions[questionIndex].answers[i];
+        answersEl.appendChild(choiceBtn);
+    }
+
+}
+
+// init. outside function, so I can use it in another function
+let responseStatus;
+function btnClick(btnPressed) {
+    // note: btnPressed arguement will return number 0-3 if 4 answers so we can compare
+    // save current question to variable for easier access
+    let currentQuestion = Questions[questionIndex];
+    // compare which button was pressed (index to answer) to corrent answer
+    if(currentQuestion.answers[btnPressed] !== currentQuestion.correctAnswer) {
+        timeLeft -= 20;
+        //test
+        console.log('Question Wrong')
+        // if incorrect
+        responseStatus = 'Wrong';
+
+        // in case subtract 20 brings it below 0
+        if(timeLeft < 0) {
+            timeLeft = 0;
+        }
+    } else {
+        //test
+        console.log('Question Right')
+        // if correct
+        responseStatus = 'Correct';
+
+    }
+
+    document.querySelector('#alert').textContent = `You are ${responseStatus}!`;
+    document.querySelector('#alert').setAttribute('class', 'current');
+    let timeOut = setTimeout(function() {
+        document.querySelector('#alert').setAttribute('class', 'notCurrent')
+    }, 500);
+
+
+    // move to next question after comparison
+    questionIndex++;
+
+    if(questionIndex === Questions.length) {
+        // if last question
+        end();
+    } else {
+        // render next question on page!
+        questionPage();
+    }
+}
+
+
+function end() {
+    // stop the time (if theres time left and you didnt LOSE)
+    // added the if statement because when it ends, if you got the last question wrong
+    // it needs to update still, without this, it'll end before the timer updates.
+    if(responseStatus==='Wrong') {
+        timeEl.textContent = timeLeft;
+    }
+    clearInterval(interval); // had id from variable
+    document.querySelector('#quiz-screen').setAttribute('class', 'notCurrent');
+    document.querySelector('#end-screen').setAttribute('class', 'current');
+}
+
